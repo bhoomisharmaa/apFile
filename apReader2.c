f3b4c3def133cd3c86b548af68f0c2a16121a54e
bhoomisharmaa <bhoomisharma404@gmail.com>
2024-11-13 01:01:10 +0530

    second version

---
+++ apReader2.c
@@ -0,0 +1,117 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int mapIndex = 0;
+
+struct Map{
+    char *key;
+    char *value;
+};
+
+struct Map2{
+    char *key;
+    struct Map *values;
+    int valueCount;
+};
+
+FILE* fileReader(char *filePath){
+    return fopen(filePath,"r");
+}
+
+void freeMap2(struct Map2 *m) {
+    for (int i = 0; i <= mapIndex; i++) {
+        free(m[i].key);
+        for (int j = 0; j < m[i].valueCount; j++) {
+            free(m[i].values[j].key);
+            free(m[i].values[j].value);
+        }
+        free(m[i].values);
+    }
+    free(m);
+}
+
+
+int keyReader(char *key, char *buffer){
+    int i = 1,index = 0;
+    key = (char*)malloc(1 * sizeof(char));
+        while(buffer[i] != '|'){
+            key = (char*)realloc(key, (index + 2) * sizeof(char)); 
+            key[index++] = buffer[i++];
+        } 
+    key[index] = '\0';
+    return i;
+}
+
+void getData(struct Map2 **m,FILE* fileptr){
+    char buffer[100];
+    struct Map2 temp;
+    while(fgets(buffer, sizeof(buffer),fileptr)){
+        int i = 1,index = 0;
+
+        // reads main key (user1/user2)
+        temp.key = (char*)malloc(1 * sizeof(char));
+        while(buffer[i] != '|'){
+            temp.key = (char*)realloc(temp.key, (index + 2) * sizeof(char)); 
+            temp.key[index++] = buffer[i++];
+        } 
+        temp.key[index] = '\0';
+
+        temp.values = (struct Map*)malloc(sizeof(struct Map));
+        struct Map t;
+        int count = 0;
+
+        while(fgets(buffer, sizeof(buffer), fileptr) && strlen(buffer) > 1){
+            i = 1,index = 0;
+            //reads key values inside main (name,email,etc)
+            t.key = (char*)malloc(1 * sizeof(char));
+            while(buffer[i] != '|'){
+                t.key = (char*)realloc(t.key, (index + 2) * sizeof(char)); 
+                t.key[index++] = buffer[i++];
+            }
+            t.key[index] = '\0';
+            
+            i++; index = 0;//i++ skips '|'
+
+            //reads values
+            t.value =  (char*)malloc(1 * sizeof(char));
+            while(buffer[i] != '~' && buffer[i] != '\0'){
+                t.value = (char*)realloc(temp.key, (index + 2) * sizeof(char)); 
+                t.value[index++] = buffer[i++];
+            }
+            t.value[index] = '\0';
+            
+            temp.valueCount++;
+            temp.values = (struct Map*)realloc(temp.values, temp.valueCount * sizeof(struct Map));
+            temp.values[temp.valueCount - 1] = t;
+        }
+
+        // Add the current Map2 struct to the array
+        *m = (struct Map2*)realloc(*m, (mapIndex + 2) * sizeof(struct Map2));
+        (*m)[mapIndex++] = temp;
+
+    }
+}
+
+void display(struct Map2 *m){
+    if(mapIndex == -1) printf("No data");
+    else{
+        for(int i = 0; i <= mapIndex; i++){
+            printf("%s : \n",m[i].key);
+            struct Map*temp = m[i].values;
+            for(int j = 0; j < 5; j++){
+                printf("%s : %s\n",temp[j].key,temp[j].value);
+            }
+        }
+    } 
+}
+
+int main(){
+    char filePath[] = "second.ap";
+    FILE *fileptr = fileReader(filePath);
+    struct Map2 *map = (struct Map2*)malloc((sizeof(struct Map2))); 
+    getData(&map,fileptr);
+    display(map);
+    free(map);
+    return 0;
+}
